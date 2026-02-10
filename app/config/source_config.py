a564ce3bc51f57b6ea1d609ea6cb6908d10529f0
Jace Rodgers <jaransas2008@gmail.com>
2026-02-09 21:05:59 -0600

    more config

--- app/config/source_config.py
+++ app/config/source_config.py
@@ -4,7 +4,7 @@ import numpy
 import config
 
 
-class SourceConfig:
+class LocalSourceConfig:
     def __init__(self, server_config_path: str):
         self.server_config_path = server_config_path
 
@@ -19,4 +19,20 @@ class SourceConfig:
     def update_camera_constants(
         self, config_store: config.CameraConfig, path: str
     ) -> None:
-        calibration_store = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
+        camera_matrix = calibration_store.getNode("camera_matrix").mat()
+        distortion_coefficients = calibration_store.getNode("distortion_coefficients").mat()
+        calibration_store.release()
+        
+        if type(camera_matrix) == numpy.ndarray and type(distortion_coefficients) == numpy.ndarray:
+            config_store.camera_matrix = camera_matrix
+            config_store.distortion_coefficients = distortion_coefficients
+            config_store.has_config = True
+
+class NTConfigSource():
+    _init_complete: bool = False
+    _camera_id_sub: ntcore.StringSubscriber
+    _camera_resolution_width_sub: ntcore.IntergerSubscriber
+    _camera_resolution_height_sub: ntcore.IntergerSubscriber
+    _camera_auto_exposure_sub: ntcore.IntergerSubscriber
+    _camera_exposure_sub: ntcore.DoubleSubscriber
+    _camera_gain_sub: ntcore.DoubleSubscriber
